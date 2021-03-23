b7761c6314a32e3d11a06de9e4f0fa8a8ca94f21
marcoceles <marco.celestino@ovolab.com>
2021-03-23 16:42:37 +0100

    defined APIClient

---
+++ stargazer/stargazer/Classes/API/APIClient.swift
@@ -0,0 +1,23 @@
+//
+//  APIClient.swift
+//  stargazer
+//
+//  Created by Marco Celestino on 23/03/2021.
+//
+
+import Foundation
+import Alamofire
+
+class APIClient{
+
+    public static let shared = APIClient()
+
+    private init(){}
+
+    func makeAPICall<O : Decodable>(_ endpoint: APIEndpoint, responseClass: O.Type, completion: @escaping (Result<O, AFError>) -> Void) -> Void {
+        let request = AF.request(endpoint, method: endpoint.method).validate(statusCode: 200..<300)
+        request.responseDecodable { (response) in
+            completion(response.result)
+        }
+    }
+}
